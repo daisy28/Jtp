70283a2e797975336dfa94ffe633f8686d93807e
Gloria <ubahgloria16@gmail.com>
2023-09-18 12:58:39 +0100

    Completed color picker

---
+++ projects/color picker/script.d.ts
@@ -0,0 +1,6 @@
+declare const colorCode: Element;
+declare const btns: NodeListOf<Element>;
+declare const colors: string[];
+declare const hexColor: (string | number)[];
+declare const getRandomColors: () => number;
+declare const getRandomHexColors: () => number;
--- projects/color picker/script.js
+++ projects/color picker/script.js
@@ -1,2 +1,38 @@
 "use strict";
-console.log("hello world");
+const colorCode = document.querySelector(".color_name");
+const btns = document.querySelectorAll(".btn");
+const colors = [
+    "chocolate",
+    "cadetblue",
+    "brown",
+    "cornflowerblue",
+    "darkcyan",
+    "darkolivegreen",
+    "darkblue",
+    "darkslateblue",
+    "mediumvioletred",
+    "rosybrown",
+];
+const hexColor = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const getRandomColors = () => {
+    return Math.floor(Math.random() * colors.length);
+};
+const getRandomHexColors = () => {
+    return Math.floor(Math.random() * hexColor.length);
+};
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (btn.classList.contains("color_btn")) {
+            colorCode.innerHTML = colors[getRandomColors()];
+            window.document.body.style.backgroundColor = colors[getRandomColors()];
+        }
+        else {
+            let hexCode = "#";
+            for (let i = 0; i < 6; i++) {
+                hexCode += hexColor[getRandomHexColors()];
+                colorCode.innerHTML = hexCode;
+                window.document.body.style.backgroundColor = hexCode;
+            }
+        }
+    });
+});
