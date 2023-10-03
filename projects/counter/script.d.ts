5cce06e7ecd35347f23f10e988d8a3c20707eb22
Gloria <ubahgloria16@gmail.com>
2023-10-03 15:30:08 +0100

    Feat: Counter

---
+++ projects/counter/script.d.ts
@@ -0,0 +1,6 @@
+declare const counterBtn: NodeListOf<Element>;
+declare const counter: Element;
+declare const saveCount: Element;
+declare const totalCount: Element;
+declare let count: number;
+declare let total: number;
---
+++ projects/counter/script.ts
@@ -0,0 +1,42 @@
+const counterBtn = document.querySelectorAll(".btn");
+const counter: Element = document.querySelector("#number")!;
+const saveCount: Element = document.querySelector("#save_count")!;
+const totalCount: Element = document.querySelector("#total_count")!;
+let count = 0;
+let total = 0;
+
+counterBtn.forEach((btns) => {
+  btns.addEventListener("click", () => {
+    if (btns.id.includes("btn_increment")) {
+      count++;
+      counter.style.color = "green";
+      counter.innerHTML = `${count}`;
+    } else if (btns.id.includes("btn_decrement")) {
+      count--;
+      counter.style.color = "crimson";
+      counter.innerHTML = `${count}`;
+    } else if (btns.id.includes("btn_reset")) {
+      count = 0;
+      total = count;
+      counter.style.color = "black";
+      totalCount.style.color = `black`;
+      saveCount.style.color = `black`;
+      counter.innerHTML = `${count}`;
+      totalCount.innerHTML = `Total: ${total}`;
+      saveCount.textContent = `Save: `;
+    } else {
+      total += count;
+      saveCount.style.color = `goldenrod`;
+      count < 0
+        ? (saveCount.textContent += `(${count}) - `)
+        : (saveCount.textContent += `${count} - `);
+      total < 0
+        ? (totalCount.style.color = `red`)
+        : (totalCount.style.color = `blue`);
+      totalCount.textContent = `Total: ${total}`;
+      count = 0;
+      counter.innerHTML = `${count}`;
+      counter.style.color = `black`;
+    }
+  });
+});
