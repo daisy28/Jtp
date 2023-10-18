8f220f92abafa27797897b5fda798194550e329c
Gloria <ubahgloria16@gmail.com>
2023-10-18 15:21:09 +0100

    Bug fix: Fixed scroll behaviour

--- projects/scroll/script.js
+++ projects/scroll/script.js
@@ -3,21 +3,21 @@ const navBar = document.querySelector(".nav");
 const menuToggle = document.querySelector(".menu_toggle");
 const menuLink = document.querySelector(".menu_links");
 const topBtn = document.querySelector(".top_btn");
+const scrollLinks = document.querySelectorAll(".scroll_link");
 const date = document.querySelector(".date");
+const navHeight = navBar.getBoundingClientRect().height;
+const menuLinkHeight = menuLink.getBoundingClientRect().height;
 date.innerHTML = `${new Date().getFullYear()}`;
 menuToggle.addEventListener("click", () => {
     menuLink.classList.toggle("show_menu_links");
 });
 const links = menuLink.querySelectorAll("a");
-links.forEach(link => {
+links.forEach((link) => {
     link.addEventListener("click", () => {
         menuLink.classList.remove("show_menu_links");
     });
 });
-const navHeight = navBar.getBoundingClientRect().height;
-// console.log(navHeight)
 window.addEventListener("scroll", () => {
-    console.log(navHeight);
     if (scrollY >= 300) {
         topBtn.classList.add("show_top_btn");
     }
@@ -31,4 +31,22 @@ window.addEventListener("scroll", () => {
         navBar.classList.remove("fixed_nav");
     }
 });
-// console.log(navHeight)
+scrollLinks.forEach((links) => {
+    links.addEventListener("click", (e) => {
+        e.preventDefault();
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        const fixedNav = navBar.classList.contains("fixed_nav");
+        let position = element.offsetTop - navHeight;
+        if (!fixedNav) {
+            position -= navHeight + 68;
+        }
+        if (navHeight > 99) {
+            position = position + menuLinkHeight;
+        }
+        window.scrollTo({
+            left: 0,
+            top: position,
+        });
+    });
+});
--- projects/scroll/script.ts
+++ projects/scroll/script.ts
@@ -2,37 +2,54 @@ const navBar: HTMLElement = document.querySelector(".nav")!;
 const menuToggle: HTMLElement = document.querySelector(".menu_toggle")!;
 const menuLink: HTMLElement = document.querySelector(".menu_links")!;
 const topBtn: HTMLElement = document.querySelector(".top_btn")!;
+const scrollLinks: NodeListOf<Element> = document.querySelectorAll(".scroll_link")!;
 const date: HTMLElement = document.querySelector(".date")!;
+const navHeight = navBar.getBoundingClientRect().height;
+const menuLinkHeight = menuLink.getBoundingClientRect().height;
 
 date.innerHTML = `${new Date().getFullYear()}`;
+
 menuToggle.addEventListener("click", () => {
-     menuLink.classList.toggle("show_menu_links");
+  menuLink.classList.toggle("show_menu_links");
 });
 
 const links = menuLink.querySelectorAll("a");
-links.forEach(link => {
-     link.addEventListener("click", () => {
-          menuLink.classList.remove("show_menu_links");
-     });
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    menuLink.classList.remove("show_menu_links");
+  });
 });
 
-const navHeight = navBar.getBoundingClientRect().height;
-// console.log(navHeight)
 window.addEventListener("scroll", () => {
-console.log(navHeight)
+  if (scrollY >= 300) {
+    topBtn.classList.add("show_top_btn");
+  } else {
+    topBtn.classList.remove("show_top_btn");
+  }
 
-     if (scrollY >= 300) {
-          topBtn.classList.add("show_top_btn");
-     } else {
-          topBtn.classList.remove("show_top_btn");
-     }
+  if (scrollY > navHeight) {
+    navBar.classList.add("fixed_nav");
+  } else {
+    navBar.classList.remove("fixed_nav");
+  }
+});
 
-     if (scrollY > navHeight) {
-          navBar.classList.add("fixed_nav");
-     } else {
-          navBar.classList.remove("fixed_nav");
-     }
-
-})
-// console.log(navHeight)
-
+scrollLinks.forEach((links) => {
+  links.addEventListener("click", (e) => {
+    e.preventDefault();
+    const id = e.currentTarget!.getAttribute("href").slice(1);
+    const element: HTMLElement = document.getElementById(id)!;
+    const fixedNav = navBar.classList.contains("fixed_nav");
+    let position = element.offsetTop - navHeight;
+    if (!fixedNav) {
+      position -= navHeight + 68;
+    }
+    if (navHeight > 99) {
+      position = position + menuLinkHeight;
+    }
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+  });
+});
