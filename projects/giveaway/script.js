3cec9966f583c5df8701058b92a752ae0a8921e1
Gloria <ubahgloria16@gmail.com>
2023-11-03 13:34:29 +0100

    Bug fix: Fixed giveaway elapse time

--- projects/giveaway/script.js
+++ projects/giveaway/script.js
@@ -24,11 +24,12 @@ const weekdays = [
 ];
 const giveawayInfo = document.querySelector(".giveaway_info_text h4");
 const countdown = document.querySelectorAll(".countdown p");
-const futureDate = new Date(2023, 10, 20, 12, 30, 0, 0);
-const currentDate = new Date();
-const timeDifference = futureDate.getTime() - currentDate.getTime();
+const expiryInfo = document.querySelector(".giveaway_container p");
+const futureDate = new Date(2023, 10, 23, 12, 30, 0, 0);
 giveawayInfo.innerHTML = `giveaway ends on ${weekdays[futureDate.getDay()]}, ${futureDate.getDate()} ${months[futureDate.getMonth()]} ${futureDate.getFullYear()} ${futureDate.getHours()}:${futureDate.getMinutes()} <span>pm</span>`;
 const countDown = () => {
+    const currentDate = new Date();
+    const timeDifference = futureDate.getTime() - currentDate.getTime();
     const oneDay = 1000 * 60 * 60 * 24;
     const oneHour = 1000 * 60 * 60;
     const oneMin = 1000 * 60;
@@ -37,9 +38,8 @@ const countDown = () => {
     const minutes = Math.floor((timeDifference % oneHour) / oneMin);
     let seconds = Math.floor((timeDifference % oneMin) / 1000);
     const timeFormat = (item) => {
-        if (item < 10) {
-            console.log(Number(`0${item}`));
-            return item = item;
+        if (typeof item === "number" && item < 10) {
+            item = `0` + item;
         }
         return item;
     };
@@ -47,7 +47,13 @@ const countDown = () => {
     countdown.forEach((item, index) => {
         item.innerHTML = `${timeFormat(values[index])}`;
     });
+    if (timeDifference < 0) {
+        clearInterval(tick);
+        expiryInfo.innerHTML += `Oooops! Giveaway time has elapsed.`;
+        countdown.forEach((item) => {
+            item.innerHTML = `00`;
+        });
+    }
 };
-setInterval(countDown, 1000);
+let tick = setInterval(countDown, 1000);
 countDown();
-// console.log(days, hours, minutes, seconds)
--- projects/giveaway/script.ts
+++ projects/giveaway/script.ts
@@ -1,69 +1,72 @@
 const months = [
-     "January",
-     "February",
-     "March",
-     "April",
-     "May",
-     "June",
-     "July",
-     "August",
-     "September",
-     "October",
-     "November",
-     "December",
-]
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 const weekdays = [
-     "sunday",
-     "monday",
-     "tuesday",
-     "wednesday",
-     "thursday",
-     "friday",
-     "saturday",
-]
-const giveawayInfo: HTMLElement = document.querySelector(".giveaway_info_text h4")!;
-const countdown: NodeListOf<Element> = document.querySelectorAll(".countdown p")!;
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+const giveawayInfo: HTMLElement = document.querySelector(
+  ".giveaway_info_text h4"
+)!;
+const countdown: NodeListOf<Element> =
+  document.querySelectorAll(".countdown p")!;
+const expiryInfo: HTMLElement = document.querySelector(
+  ".giveaway_container p"
+)!;
+const futureDate = new Date(2023, 10, 23, 12, 30, 0, 0);
 
-const futureDate = new Date(2023, 10, 20, 12, 30, 0, 0);
-const currentDate = new Date();
-const timeDifference = futureDate.getTime() - currentDate.getTime();
-
-giveawayInfo.innerHTML = `giveaway ends on ${weekdays[futureDate.getDay()]}, ${futureDate.getDate()} ${months[futureDate.getMonth()]} ${futureDate.getFullYear()} ${futureDate.getHours()}:${futureDate.getMinutes()} <span>pm</span>`;
+giveawayInfo.innerHTML = `giveaway ends on ${
+  weekdays[futureDate.getDay()]
+}, ${futureDate.getDate()} ${
+  months[futureDate.getMonth()]
+} ${futureDate.getFullYear()} ${futureDate.getHours()}:${futureDate.getMinutes()} <span>pm</span>`;
 
 const countDown = () => {
-     const oneDay = 1000 * 60 * 60 * 24;
-     const oneHour = 1000 * 60 * 60;
-     const oneMin = 1000 * 60;
-     
-     const days = Math.floor(timeDifference / oneDay);
-     const hours = Math.floor((timeDifference % oneDay) / oneHour);
-     const minutes = Math.floor((timeDifference % oneHour) / oneMin);
-     let seconds = Math.floor((timeDifference % oneMin) / 1000);
+  const currentDate = new Date();
+  const timeDifference = futureDate.getTime() - currentDate.getTime();
+  const oneDay = 1000 * 60 * 60 * 24;
+  const oneHour = 1000 * 60 * 60;
+  const oneMin = 1000 * 60;
+  const days = Math.floor(timeDifference / oneDay);
+  const hours = Math.floor((timeDifference % oneDay) / oneHour);
+  const minutes = Math.floor((timeDifference % oneHour) / oneMin);
+  let seconds = Math.floor((timeDifference % oneMin) / 1000);
 
-     const timeFormat = (item: number) => {
-          if (item < 10) {
-               console.log(Number(`0${item}`))
-               return item = item
-          }
-          return item
-     };
+  const timeFormat = (item: number | string) => {
+    if (typeof item === "number" && item < 10) {
+      item = `0` + item;
+    }
+    return item;
+  };
 
-     const values = [days, hours, minutes, seconds];
-     
-     countdown.forEach((item, index) => {
-          item.innerHTML = `${timeFormat(values[index])}`;
-     });
-}
-setInterval(countDown, 1000)
+  const values = [days, hours, minutes, seconds];
+  countdown.forEach((item, index) => {
+    item.innerHTML = `${timeFormat(values[index])}`;
+  });
+  if (timeDifference < 0) {
+    clearInterval(tick);
+    expiryInfo.innerHTML += `Oooops! Giveaway time has elapsed.`;
+    countdown.forEach((item) => {
+      item.innerHTML = `00`;
+    });
+  }
+};
 
-
-
+let tick = setInterval(countDown, 1000);
 countDown();
-
-
-
-
-
-
-
-// console.log(days, hours, minutes, seconds)
