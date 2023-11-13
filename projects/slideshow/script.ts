d8c0303b8b406f5c06f47c2d199d54031d636985
Gloria <ubahgloria16@gmail.com>
2023-11-13 12:25:54 +0100

    Setup for the slideshow

---
+++ projects/slideshow/script.ts
@@ -0,0 +1,37 @@
+const usersData = [
+  {
+    img: "../assets/man1.jpg",
+    name: "George Williams",
+    occupation: "Graphics & UI/UX Designer",
+    testimonial:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam molestias neque voluptate laudantium repellendus velit, minima harum mollitia sint veniam nam cupiditate tenetur qui, optio quae quam inventore, nulla beatae!",
+  },
+  {
+    img: "../assets/man2.jpg",
+    name: "Jerry Luis",
+    occupation: "Cyber Security Engineer",
+    testimonial:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam molestias neque voluptate laudantium repellendus velit, minima harum mollitia sint veniam nam cupiditate tenetur qui, optio quae quam inventore, nulla beatae!",
+  },
+  {
+    img: "../assets/woman1.jpg",
+    name: "Jordan Clay",
+    occupation: "Software Developer",
+    testimonial:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam molestias neque voluptate laudantium repellendus velit, minima harum mollitia sint veniam nam cupiditate tenetur qui, optio quae quam inventore, nulla beatae!",
+  },
+  {
+    img: "../assets/man3.jpg",
+    name: "David Beckham",
+    occupation: "Data Analyst",
+    testimonial:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam molestias neque voluptate laudantium repellendus velit, minima harum mollitia sint veniam nam cupiditate tenetur qui, optio quae quam inventore, nulla beatae!",
+  },
+  {
+    img: "../assets/woman2.jpg",
+    name: "Jane Love",
+    occupation: "UI/UX Designer",
+    testimonial:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam molestias neque voluptate laudantium repellendus velit, minima harum mollitia sint veniam nam cupiditate tenetur qui, optio quae quam inventore, nulla beatae!",
+  },
+];
