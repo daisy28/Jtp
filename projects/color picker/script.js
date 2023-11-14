3ce655354ae08e7828bb530078c3d4b5957d5e0e
Gloria <ubahgloria16@gmail.com>
2023-11-14 13:58:06 +0100

    Color picker: Minor fix

--- projects/color picker/script.js
+++ projects/color picker/script.js
@@ -12,6 +12,8 @@ const colors = [
     "darkslateblue",
     "mediumvioletred",
     "rosybrown",
+    "darkred",
+    "teal"
 ];
 const hexColor = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const getRandomColors = () => {
